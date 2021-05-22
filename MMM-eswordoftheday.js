5f0f72b10b15c6645e978edddd66433de74e00e8
Daniel Windsor <dw.accounts@protonmail.com>
2021-05-22 20:28:02 +1200

    Init commit

---
+++ MMM-eswordoftheday.js
@@ -0,0 +1,65 @@
+Module.register("MMM-eswordoftheday", {
+  defaults: {
+    updateInterval: 86400000,
+    retryDelay: 5000,
+  },
+
+  start: function() {
+    Log.info(`Starting module: ${this.name}`)
+
+    this.apiData = null
+
+    this.sendSocketNotification("MMM-eswordoftheday-GET_WORD", null)
+
+    const self = this
+    setTimeout(function() {
+      self.sendSocketNotification("MMM-eswordoftheday-GET_WORD", null)
+    }, this.config.updateInterval)
+  },
+
+  getHeader: function() {
+    return "Spanish Word of the Day"
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "MMM-eswordoftheday-RETURN_WORD") {
+      console.log('recieved: ', payload)
+      this.apiData = payload
+      this.updateDom()
+    }
+  },
+
+  getDom: function() {
+    console.log(this.apiData)
+		const wrapper = document.createElement("div");
+
+    if (this.apiData) {
+      const word = document.createElement('div')
+      word.innerHTML = this.apiData.word
+
+      const translation = document.createElement('div')
+      translation.innerHTML = this.apiData.translation
+
+      const list = document.createElement('ol')
+      for (const key in this.apiData.examples) {
+        const listItem = document.createElement('li')
+
+        const es = document.createElement('div')
+        es.innerHTML = this.apiData.examples[key].spanish
+
+        const en = document.createElement('div')
+        en.innerHTML = this.apiData.examples[key].english
+
+        listItem.appendChild(es)
+        listItem.appendChild(en)
+        list.appendChild(listItem)
+      }
+      
+      wrapper.appendChild(word)
+      wrapper.appendChild(translation)
+      wrapper.appendChild(list)
+    }
+
+    return wrapper
+  }
+})
---
+++ node_helper.js
@@ -0,0 +1,45 @@
+const NodeHelper = require("node_helper")
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+module.exports = NodeHelper.create({
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "MMM-eswordoftheday-GET_WORD") {
+      const url = "https://www.spanishdict.com/wordoftheday";
+
+      axios.get(url).then(({ data }) => {
+        const $ = cheerio.load(data);
+  
+      const container = $(".entry--1qTEOKKt")[0];
+
+        const word = $(container).find("h3");
+        const translation = word.next();
+
+        const exampleContainer = $(container).find('ol').first().children()
+        const firstExample = exampleContainer.first().children()
+        const secondExample = exampleContainer.next().children()
+
+        const examples = {
+          one: {
+            spanish: firstExample.first().text(),
+            english: firstExample.next().text()
+          },
+          two: {
+            spanish: secondExample.first().text(),
+            english: secondExample.next().text()
+          }
+        }
+
+        console.log(examples)
+  
+        const processed = { 
+          word: word.text(),
+          translation: translation.text(),
+          examples: examples
+        }
+
+        this.sendSocketNotification("MMM-eswordoftheday-RETURN_WORD", processed)
+      })
+    }
+  }
+})
