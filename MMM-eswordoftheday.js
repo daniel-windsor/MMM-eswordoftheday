691ac37c585679f8f4dffacb969713932f7b2211
GeeYouEss <gabrielvoyer@gmail.com>
2025-02-23 14:24:02 -0500

    Fixed getDom to use div instead of li for examples with CSS spacing

--- MMM-eswordoftheday.js
+++ MMM-eswordoftheday.js
@@ -9,11 +9,7 @@ Module.register("MMM-eswordoftheday", {
   start: function () {
     Log.info(`Starting module: ${this.name}`)
     this.apiData = null
-
-    // Fetch initial data
     this.sendSocketNotification("MMM-eswordoftheday-GET_WORD", {})
-
-    // Fetch every 6 hours (or custom interval)
     setInterval(() => {
       this.sendSocketNotification("MMM-eswordoftheday-GET_WORD", {})
     }, this.config.updateInterval)
@@ -23,6 +19,10 @@ Module.register("MMM-eswordoftheday", {
     return "Spanish Word of the Day"
   },
 
+  getStyles: function () {
+    return ["MMM-eswordoftheday.css"]
+  },
+
   socketNotificationReceived: function (notification, payload) {
     if (notification === "MMM-eswordoftheday-RETURN_WORD") {
       Log.info("Received:", payload)
@@ -30,43 +30,40 @@ Module.register("MMM-eswordoftheday", {
       this.updateDom()
     }
   },
-  getStyles: function () {
-    return ["MMM-eswordoftheday.css"]
-  },
-  
+
   getDom: function () {
     const wrapper = document.createElement("div")
-  
+
     if (this.apiData) {
       const word = document.createElement("div")
       word.innerHTML = this.apiData.word
       word.className = "bold large"
       wrapper.appendChild(word)
-  
+
       const translation = document.createElement("div")
       translation.innerHTML = this.apiData.translation
       wrapper.appendChild(translation)
-  
+
       if (this.config.showExamples && this.apiData.spanishExample) {
         const exampleContainer = document.createElement("div")
-        exampleContainer.className = "small example-spacing" // Add custom class
-  
+        exampleContainer.className = "small example-spacing"
+
         const es = document.createElement("div")
         es.innerHTML = this.apiData.spanishExample || "."
         exampleContainer.appendChild(es)
-  
+
         if (this.config.showExampleTranslations && this.apiData.englishExample) {
           const en = document.createElement("div")
           en.innerHTML = this.apiData.englishExample || "."
           exampleContainer.appendChild(en)
         }
-  
+
         wrapper.appendChild(exampleContainer)
       }
     } else {
       wrapper.innerHTML = "Loading..."
     }
-  
+
     return wrapper
   },
 })
