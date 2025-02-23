e1dad04461be7514f913404cd3a21d7231375063
GeeYouEss <gabrielvoyer@gmail.com>
2025-02-23 14:08:34 -0500

    Added padding to examples via CSS

--- MMM-eswordoftheday.js
+++ MMM-eswordoftheday.js
@@ -30,43 +30,43 @@ Module.register("MMM-eswordoftheday", {
       this.updateDom()
     }
   },
-
+  getStyles: function () {
+    return ["MMM-eswordoftheday.css"]
+  },
+  
   getDom: function () {
     const wrapper = document.createElement("div")
-
+  
     if (this.apiData) {
       const word = document.createElement("div")
       word.innerHTML = this.apiData.word
       word.className = "bold large"
       wrapper.appendChild(word)
-
+  
       const translation = document.createElement("div")
       translation.innerHTML = this.apiData.translation
       wrapper.appendChild(translation)
-
+  
       if (this.config.showExamples && this.apiData.spanishExample) {
-        const list = document.createElement("ul")
-        list.className = "small"
-
-        const listItem = document.createElement("li")
-
+        const exampleContainer = document.createElement("div")
+        exampleContainer.className = "small example-spacing" // Add custom class
+  
         const es = document.createElement("div")
         es.innerHTML = this.apiData.spanishExample || "."
-        listItem.appendChild(es)
-
+        exampleContainer.appendChild(es)
+  
         if (this.config.showExampleTranslations && this.apiData.englishExample) {
           const en = document.createElement("div")
           en.innerHTML = this.apiData.englishExample || "."
-          listItem.appendChild(en)
+          exampleContainer.appendChild(en)
         }
-
-        list.appendChild(listItem)
-        wrapper.appendChild(list)
+  
+        wrapper.appendChild(exampleContainer)
       }
     } else {
       wrapper.innerHTML = "Loading..."
     }
-
+  
     return wrapper
   },
 })
